6cba086a80ccc4303bba5bc79675cfc16f1a1698
william-nguyen128 <nguyenhongquan2k@gmail.com>
2023-08-06 11:47:46 +0700

    feat: Created LLM response generation function

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,17 @@ def clear_chat_history():
     st.session_state.messages = [{"role": "assistant", "content": "How may I assist you today?"}]
 st.sidebar.button('Clear Chat History', on_click=clear_chat_history)
 
+# Function for generating LLaMA2 response
+# Refactored from <https://github.com/a16z-infra/llama2-chatbot>
+def generate_llama2_response(prompt_input):
+    string_dialogue = "You are a helpful assistant. You do not respond as 'User' or pretend to be 'User'. You only respond once as 'Assistant'."
+    for dict_message in st.session_state.messages:
+        if dict_message["role"] == "user":
+            string_dialogue += "User: " + dict_message["content"] + "\\n\\n"
+        else:
+            string_dialogue += "Assistant: " + dict_message["content"] + "\\n\\n"
+    output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', 
+                           input={"prompt": f"{string_dialogue} {prompt_input} Assistant: ",
+                                  "temperature":0.1, "top_p":0.9, "max_length":512, "repetition_penalty":1})
+    return output
+
