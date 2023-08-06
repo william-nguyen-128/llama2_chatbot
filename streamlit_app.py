5fa93b8579648d678466204306845e30356fb6d3
william-nguyen128 <nguyenhongquan2k@gmail.com>
2023-08-06 11:39:57 +0700

    feat: Add sidebar with app name & replicate token checks

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,4 +5,17 @@ import os
 # App title
 st.set_page_config(page_title="🦙💬 Llama 2 Chatbot")
 
-st.write('Hello world!')
+# Replicate Credentials
+with st.sidebar:
+    st.title('🦙💬 Llama 2 Chatbot')
+    if 'REPLICATE_API_TOKEN' in st.secrets:
+        st.success('API key already provided!', icon='✅')
+        replicate_api = st.secrets['REPLICATE_API_TOKEN']
+    else:
+        replicate_api = st.text_input('Enter Replicate API token:', type='password')
+        if not (replicate_api.startswith('r8_') and len(replicate_api)==40):
+            st.warning('Please enter your credentials!', icon='⚠️')
+        else:
+            st.success('Proceed to entering your prompt message!', icon='👉')
+    st.markdown('📖 Learn how to build this app in this [blog](#link-to-blog)!')
+os.environ['REPLICATE_API_TOKEN'] = replicate_api
